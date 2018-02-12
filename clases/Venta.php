72efc54a14df63a962aa841a53f3b023f037e327
Matias Otero <matiotero_1990@hotmail.com>
2018-02-12 15:28:25 -0300

    advance

--- clases/Venta.php
+++ clases/Venta.php
@@ -4,13 +4,25 @@ class Venta{
     public $sabor;
     public $tipo;
     public $peso;
+    public $pathFoto;
 
     function ToString(){
-		return $this->mail."|".$this->sabor."|".$this->tipo."|".$this->peso."\n";
+      $objToString = $this->mail."|".$this->sabor."|".$this->tipo."|".$this->peso."\n";
+		if($this->pathFoto === NULL){
+			return $objToString."\n";
+		}
+		else {
+      $objToString = $objToString.$this->pathFoto."\n";
+      return objToString.$pathFoto."\n";
+		}
   }
   
-  public static function GuardarVenta(){
-    
+  public static function GuardarVentaTxt($arrayVentas){
+    $archivo = fopen("Ventas.txt", "a");
+    for($i = 0; $i < count($arrayVentas); $i++){
+      
+    }
   } 
+
 }
 ?>
